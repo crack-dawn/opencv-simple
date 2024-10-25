03e83cf7dbdd5e3fbe16b2fa6c79b5f7ecccdb4e
王毣然 <15997610143@163.com>
2024-10-25 20:20:05 +0800

    opencv 4.10.0 windows mingw64 简单模板 不含opencv contrib库包 ----------------- 编译配置参数如下: cmake -DWITH_OPENMP=ON -DWITH_OPENCL=ON -DWITH_OPENGL=ON -DWITH_TBB=ON -DWITH_VULKAN=ON -DWITH_QT=OFF -DBUILD_WITH_DEBUG_INFO=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_PROTOBUF=OFF -DPROTOBUF_UPDATE_FILES=ON -DINSTALL_C_EXAMPLES=OFF -DINSTALL_PYTHON_EXAMPLES=ON -DCPU_BASELINE_DISABLE=SSE3 -DCPU_BASELINE_REQUIRE=SSE2 -DOPENCV_SKIP_PYTHON_LOADER=ON -DOPENCV_ENABLE_NONFREE=ON -DBUILD_opencv_world=ON -G "MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++ ..\ cmake --build . --config Release

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+using namespace std;
+
+int main(){
+	// 读入图片
+	Mat img = imread("img.png");
+	
+	// 判断是否正确读入图片
+	if (img.empty()){
+		cout<<"图片路径错误，没有图片被读入"<<endl;
+		return 0;
+	}
+	
+	// 保存图片
+	// imwrite("another_salute.png",img);
+	
+	// 显示图片
+	imshow("salute",img);
+ 	waitKey();
+	destroyAllWindows();
+	return 0;
+}
